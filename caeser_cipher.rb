f2a83f71f5166856bebc1559d10f2bbf2f8b452d
Daniel <dthompson2340@outlook.com>
2024-11-10 21:53:01 +1100

    finished

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -20,21 +20,44 @@ def make_cipher_key()
   end
 
 
-  return cipher_key = Hash[alpha.zip(numbers_plus_five)]
+  return cipher_key = Hash[numbers_plus_five.zip(alpha)]
 end
 puts "What to Encrypt:"
 original_msg = gets.chomp
 og_key = make_og_key
 cipher = make_cipher_key
+print(cipher)
 cipher_a = []
 
 original_msg.each_char do |char|
   if char == " "
-    cipher_a.push
+    cipher_a.push(" ")
   else
     num = og_key[char]
-    cipher_msg += num.to_s
+    cipher_a.push(num)
   end
 end
 
-puts cipher_msg
+# cipher_a.map! do |num|
+#   if num == " "
+#     num = " "
+#   elsif num > 21
+#     num - 21
+#   else
+#     num += 5
+#   end
+# end
+print(cipher_a)
+encrypted_msg = ""
+
+for num in cipher_a
+  if num == " "
+    encrypted_msg += num
+  else
+    new_letter = cipher[num]
+    encrypted_msg += new_letter
+  end
+end
+
+print encrypted_msg
+
