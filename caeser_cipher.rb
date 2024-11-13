3db823d5a859a21d59036b1409b87edd49d20cfc
Daniel <dthompson2340@outlook.com>
2024-11-13 22:27:22 +1100

    Added bunlde and rubocop. Now IAW the law

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,17 +1,18 @@
-def make_og_key()
-
+# Makes original key
+def make_og_key
   alpha = ('a'..'z').to_a
   numbers = (1..26).to_a
 
-  return original_key = Hash[alpha.zip(numbers)]
+  Hash[alpha.zip(numbers)]
 end
 
-def make_cipher_key()
+# Makes +5 cypher key
+def make_cipher_key
   alpha = ('a'..'z').to_a
   numbers_plus_five = []
   numbers = (1..26).to_a
 
-  for number in numbers
+  numbers.each do |number|
     if number > 21
       numbers_plus_five.push(number - 21)
     else
@@ -19,30 +20,27 @@ def make_cipher_key()
     end
   end
 
-
-  return cipher_key = Hash[numbers_plus_five.zip(alpha)]
+  Hash[numbers_plus_five.zip(alpha)]
 end
-puts "What to Encrypt:"
+puts 'What to Encrypt:'
 original_msg = gets.chomp
 og_key = make_og_key
 cipher = make_cipher_key
-print(cipher)
 cipher_a = []
 
 original_msg.each_char do |char|
-  if char == " "
-    cipher_a.push(" ")
+  if char == ' '
+    cipher_a.push(' ')
   else
     num = og_key[char]
     cipher_a.push(num)
   end
 end
 
+encrypted_msg = ''
 
-encrypted_msg = ""
-
-for num in cipher_a
-  if num == " "
+cipher_a.each do |num|
+  if num == ' '
     encrypted_msg += num
   else
     new_letter = cipher[num]
@@ -51,4 +49,3 @@ for num in cipher_a
 end
 
 print encrypted_msg
-
